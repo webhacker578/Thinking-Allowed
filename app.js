9dd4167f1bc42c695d4e110d2e9d5ce602eae310
Kunj Dhola <kunjdhola8a@gmail.com>
2021-04-29 11:33:21 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $('.navbar-toggler').on('click', function () {
+    $('.navbar-toggle-icon').toggleClass('open');
+  });
+});
+// const logo = document.querySelectorAll("#logo text");
+
+// for (let i = 0; i<logo.length; i++) {
+//     console.log('Letter ${i} is ${logo{i}.getTotalLenght()}');
+// }
