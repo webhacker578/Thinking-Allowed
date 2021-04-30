39f6ba4df2d62edad62b3e641c080ce55cd6f02f
Kunj Dhola <kunjdhola8a@gmail.com>
2021-04-30 15:09:48 +0530

    'responsive'

--- app.js
+++ app.js
@@ -8,4 +8,3 @@ $(document).ready(function () {
 // for (let i = 0; i<logo.length; i++) {
 //     console.log('Letter ${i} is ${logo{i}.getTotalLenght()}');
 // }
-<script type='text/javascript'>document.addEventListener('DOMContentLoaded', function () {window.setTimeout(document.querySelector('svg').classList.add('animated'),1000);})</script>
