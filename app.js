fbb76fd330caab9c606d292c58d942c8deaf609c
Kunj Dhola <kunjdhola8a@gmail.com>
2021-05-04 18:42:48 +0530

    added backend for contact

--- app.js
+++ app.js
@@ -10,9 +10,35 @@ $(function () {
     $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
   });
 });
-// let form = document.querySelector(".container")
-// let button = document.querySelector(".submit-btn");
-// button.addEventListener("click",function(e){
-//   e.preventDefault();
-//   alert("Hello")
-// })
+var firebaseConfig = {
+  apiKey: "AIzaSyDAj-MVIfgsrc5ZlmFgQWm5cUxfQdQPVsQ",
+  authDomain: "thinking-allowed.firebaseapp.com",
+  projectId: "thinking-allowed",
+  storageBucket: "thinking-allowed.appspot.com",
+  messagingSenderId: "443277934701",
+  appId: "1:443277934701:web:a4a0044866540bda07ff64",
+  measurementId: "G-FHW74TTRVN"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+let contactInfo = firebase.database().ref("contacts");
+
+let form  = document.querySelector("#contact");
+form.addEventListener("submit",function(e){
+  e.preventDefault();
+  let name = document.querySelector(".name").value;
+  let email = document.querySelector(".email").value;
+  let phone = document.querySelector(".phone").value;
+  let message = document.querySelector(".message").value;
+  
+  let newContactInfo = contactInfo.push();
+  form.reset();
+
+  newContactInfo.set({
+    username: name,
+    email: email,
+    phone: phone,
+    message: message,
+  });
+})
