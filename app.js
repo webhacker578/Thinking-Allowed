793ad65b1a21b655281a5af2bec39d57c21df446
Kunj Dhola <kunjdhola8a@gmail.com>
2021-06-17 18:56:56 +0530

    /

--- app.js
+++ app.js
@@ -10,44 +10,18 @@ $(function () {
     $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
   });
 });
-var firebaseConfig1 = {
-  apiKey: "AIzaSyDAj-MVIfgsrc5ZlmFgQWm5cUxfQdQPVsQ",
-  authDomain: "thinking-allowed.firebaseapp.com",
-  projectId: "thinking-allowed",
-  storageBucket: "thinking-allowed.appspot.com",
-  messagingSenderId: "443277934701",
-  appId: "1:443277934701:web:a4a0044866540bda07ff64",
-  measurementId: "G-FHW74TTRVN"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig1);
-firebase.analytics();
-let contactInfo = firebase.database().ref("contacts");
 
-let form  = document.querySelector("#contact");
-form.addEventListener("submit",function(e){
-  e.preventDefault();
-  let name = document.querySelector(".name").value;
-  let email = document.querySelector(".email").value;
-  let phone = document.querySelector(".phone").value;
-  let message = document.querySelector(".message").value;
-  
-  let newContactInfo = contactInfo.push();
-  form.reset();
-  document.querySelector('.alert').style.display = 'block';
-
-  setTimeout(function(){
-    document.querySelector('.alert').style.display = 'none';
-  },3000);
-  newContactInfo.set({
-    username: name,
-    email: email,
-    phone: phone,
-    message: message,
-  });
+let form = document.querySelector("#contact");
+form.addEventListener("submit", function () {
+  setTimeout(function () {
+    form.reset();
+  }, 3000);
 })
-$(document).on('keyup keypress', 'form input', function(e) {
-  if(e.which == 13) {
+
+
+
+$(document).on('keyup keypress', 'form input', function (e) {
+  if (e.which == 13) {
     e.preventDefault();
     return false;
   }
--- register.js
+++ register.js
@@ -1,44 +1,6 @@
-var firebaseConfig2 = {
-  apiKey: "AIzaSyBrCytg9YG8bB6eWmxqiw70vsxH5xn_5gg",
-  authDomain: "thinking-register.firebaseapp.com",
-  databaseURL: "https://thinking-register-default-rtdb.firebaseio.com",
-  projectId: "thinking-register",
-  storageBucket: "thinking-register.appspot.com",
-  messagingSenderId: "193097788609",
-  appId: "1:193097788609:web:3138b0567eccd18f9eb840",
-  measurementId: "G-X7MFBX6RV7"
-};
-firebase.initializeApp(firebaseConfig2);
-let registerInfo = firebase.database().ref("registered");
-
-let registerForm = document.querySelector("#app-form");
+let registerForm = document.querySelector(".app-form");
 registerForm.addEventListener("submit", function (e) {
-  // e.preventDefault();
-  let regName = document.querySelector("#regname").value;
-  let schoolName = document.querySelector("#regschool").value;
-  let schoolEmail = document.querySelector("#regemail").value;
-  let contact = document.querySelector("#regcontact").value;
-
-  let newRegInfo = registerInfo.push();
-
-  setTimeout(function () {
-    registerForm.reset();
-  }, 3000);
-
-  newRegInfo.set({
-    regName: regName,
-    schoolName: schoolName,
-    schoolEmail: schoolEmail,
-    contact: contact,
-  });
-
-  
-
-
+    setTimeout(function () {
+        registerForm.reset();
+    }, 3000);
 })
-$(document).on('keyup keypress', 'form input', function (e) {
-  if (e.which == 13) {
-    e.preventDefault();
-    return false;
-  }
-});
